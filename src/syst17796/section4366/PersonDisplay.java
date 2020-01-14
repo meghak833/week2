eca3aff718bd87327fff537f80076eb78fe91d7c
megha <megha.patel2@sheridancollege.ca>
2020-01-14 13:51:29 -0500

    first commit

---
+++ src/syst17796/section4366/PersonDisplay.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syst17796.section4366;
+
+/**
+ *
+ * @author Megha, Winter 2020
+ */
+public class PersonDisplay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Person p=new Person();
+        p.setName("Megha");
+
+        System.out.println("name: "+p.getName());
+    }
+    
+}
